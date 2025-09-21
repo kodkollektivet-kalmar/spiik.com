b54107a45cdcc71cbfca4ae22810c939c585abfb
svz <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-09-21 10:37:35 +0200

    Install packages

---
+++ src/env.js
@@ -0,0 +1,22 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+const env = createEnv({
+	server: {
+		NODE_ENV: z
+			.enum(["development", "test", "production"])
+			.default("development"),
+		PAYLOAD_SECRET: z.string(),
+		DATABASE_URL: z.string(),
+	},
+	client: {
+		// Public environment variables here...
+	},
+	runtimeEnv: {
+		NODE_ENV: process.env.NODE_ENV,
+		PAYLOAD_SECRET: process.env.PAYLOAD_SECRET,
+		DATABASE_URL: process.env.DATABASE_URL,
+	},
+});
+
+export { env };
