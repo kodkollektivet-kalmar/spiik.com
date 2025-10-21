a66e1b7b0a319869b23dfb5e33260af28510fcee
svz <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-10-21 11:36:55 +0200

    Create migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251019_150202 from './20251019_150202';
+import * as migration_20251021_093631 from './20251021_093631';
 
 export const migrations = [
   {
     up: migration_20251019_150202.up,
     down: migration_20251019_150202.down,
-    name: '20251019_150202'
+    name: '20251019_150202',
+  },
+  {
+    up: migration_20251021_093631.up,
+    down: migration_20251021_093631.down,
+    name: '20251021_093631'
   },
 ];
